b9f10c0830a69d2c02963b6cbd379a2718330955
juniobagus <juniobagus@gmail.com>
2019-04-04 11:37:08 +0700

    Adding fonts and scene
    Adding fonts and scene, some scene just for debugging

---
+++ Assets/DataFiles/Scripts/LoadScene.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+    public void LoadMulai()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+
+    public void LoadPetunjuk()
+    {
+        Debug.Log("Load Petunjuk");
+    }
+
+    public void LoadTentang()
+    {
+        Debug.Log("Load Tentang");
+    }
+
+    public void LoadExit()
+    {
+        Debug.Log("Load Exit");
+    }
+}
---
+++ Assets/DataFiles/Scripts/Walk.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walk : MonoBehaviour {
+    public int speed = 3;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
